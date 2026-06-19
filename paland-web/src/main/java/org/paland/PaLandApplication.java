3503935d700228f615656409716e9c4f92e4f29f
ChenJun <cj970720@gmail.com>
2026-06-19 16:46:41 +0800

    feat(project): 初始化项目结构和配置
    - 添加 .gitignore 文件配置忽略规则
    - 创建 Maven 多模块项目的 pom.xml 配置文件
    - 添加 paland-web 模块的基础配置
    - 配置 Spring Boot 和 Lombok 依赖管理
    - 创建 Spring Boot 主应用程序类

---
+++ paland-web/src/main/java/org/paland/PaLandApplication.java
@@ -0,0 +1,17 @@
+package org.paland;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class PaLandApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaLandApplication.class, args);
+        log.info("===================== PaLand 启动成功 =====================");
+    }
+}
