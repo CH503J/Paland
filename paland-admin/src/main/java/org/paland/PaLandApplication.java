9cc0384c72e498d0abe7532d4f0ac5a9afc1545d
ChenJun <cj970720@gmail.com>
2026-06-22 01:00:40 +0800

    chore(app): 添加定时任务注释说明
    - 添加清理超过24小时未使用缓存、镜像、容器的定时任务注释

--- paland-admin/src/main/java/org/paland/PaLandApplication.java
+++ paland-admin/src/main/java/org/paland/PaLandApplication.java
@@ -10,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan(value = "org.paland.system.module", annotationClass = Mapper.class)
 public class PaLandApplication {
+
+    // 测试 清理超过24小时未使用的缓存、镜像、容器 定时任务
     public static void main(String[] args) {
         SpringApplication.run(PaLandApplication.class, args);
         log.info("===================== PaLand 启动成功 =====================");
